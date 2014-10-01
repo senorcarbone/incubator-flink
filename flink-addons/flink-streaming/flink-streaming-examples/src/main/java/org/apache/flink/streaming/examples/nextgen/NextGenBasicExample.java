fad9dc1cab54d4927865d89eb165ee319fc4ef28
Jonas Traub (powibol) <jon@s-traub.com>
2014-10-01 17:36:43 +0200

    Prototyping: Removed unused imports, eliminated warnings

--- flink-addons/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/nextgen/NextGenBasicExample.java
+++ flink-addons/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/nextgen/NextGenBasicExample.java
@@ -1,7 +1,5 @@
 package org.apache.flink.streaming.examples.nextgen;
 
-import java.util.LinkedList;
-
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
