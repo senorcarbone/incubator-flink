95bdf74273221a6c9011004a74f6a0c071e23606
Jonas Traub (powibol) <jon@s-traub.com>
2014-10-01 10:20:12 +0200

    Prototyping: Create interface for policies (Trigger & Eviction) and enum for the window types (Sliding/Tumbling)

---
+++ flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/invokable/operator/NextGenPolicy.java
@@ -0,0 +1,19 @@
+package org.apache.flink.streaming.api.invokable.operator;
+
+public interface NextGenPolicy<DATA,FLAG> {
+
+	/**
+	 * Proves and returns if a new window should be started
+	 * @param datapoint the data point in the stream which arrived
+	 * @return true if the current windows should be closed, otherwise false.
+	 *         In true case the given data point should be part	of the
+	 *         next window and should not be included in the current one.
+	 */
+	public boolean addDataPoint(DATA datapoint);
+	
+	/**
+	 * Returns a flag which can be used to uniquely identify this policy 
+	 * @return flag which can be used to uniquely identify this policy
+	 */
+	public FLAG getFlag();
+}
---
+++ flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/invokable/operator/NextGenWindowType.java
@@ -0,0 +1,6 @@
+package org.apache.flink.streaming.api.invokable.operator;
+
+public enum NextGenWindowType {
+	TUMBLING,
+	SLIDING;
+}
