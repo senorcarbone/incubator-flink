c9ab29c0658f8989acbac321425571b67d8d0cec
Jonas Traub (powibol) <jon@s-traub.com>
2014-10-05 16:35:32 +0200

    Prototyping: Added new interface for eviction policies

---
+++ flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/invokable/operator/NextGenEvictionPolicy.java
@@ -0,0 +1,18 @@
+package org.apache.flink.streaming.api.invokable.operator;
+
+import java.io.Serializable;
+
+public interface NextGenEvictionPolicy<DATA> extends Serializable{
+
+	/**
+	 * Proves if and how many elements should be deleted from the
+	 * element buffer. The eviction takes place after the trigger
+	 * and after the call to the UDF but before the adding of the
+	 * new datapoint.
+	 * 
+	 * @param datapoint datapoint the data point which arrived
+	 * @param triggered Information whether the UDF was triggered or not
+	 * @return The number of elements to be deleted from the buffer
+	 */
+	public int addDataPoint(DATA datapoint, boolean triggered);
+}
--- flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/invokable/operator/NextGenWindowType.java
+++
@@ -1,6 +0,0 @@
-package org.apache.flink.streaming.api.invokable.operator;
-
-public enum NextGenWindowType {
-	TUMBLING,
-	SLIDING;
-}
