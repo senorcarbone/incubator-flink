c6c6d7a1f8ef06194b9a70cc8ac8ed295b966bd8
Jonas Traub (powibol) <jon@s-traub.com>
2014-10-08 13:58:29 +0200

    Finalized the type wrapper

--- flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/util/serialization/CombineTypeWrapper.java
+++ flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/util/serialization/CombineTypeWrapper.java
@@ -5,29 +5,29 @@ import java.io.IOException;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 
-public class NextGenOutTypeWrapper<OUT1,OUT2> extends TypeWrapper<Tuple2<OUT1, OUT2>>{
+public class CombineTypeWrapper<OUT1, OUT2> extends
+		TypeWrapper<Tuple2<OUT1, OUT2>> {
 
 	private static final long serialVersionUID = 1L;
-	//Info about OUT
+	// Info about OUT
 	private TypeWrapper<OUT1> outTypeWrapper1;
 	private TypeWrapper<OUT2> outTypeWrapper2;
-	
-	public NextGenOutTypeWrapper(TypeWrapper<OUT1> outTypeWrapper1,TypeWrapper<OUT2> outTypeWrapper2){
-		this.outTypeWrapper1=outTypeWrapper1;
-		this.outTypeWrapper2=outTypeWrapper2;
+
+	public CombineTypeWrapper(TypeWrapper<OUT1> outTypeWrapper1,
+			TypeWrapper<OUT2> outTypeWrapper2) {
+		this.outTypeWrapper1 = outTypeWrapper1;
+		this.outTypeWrapper2 = outTypeWrapper2;
 	}
-	
+
 	private void readObject(java.io.ObjectInputStream in) throws IOException,
-	ClassNotFoundException {
+			ClassNotFoundException {
 		in.defaultReadObject();
 		setTypeInfo();
 	}
-	
+
 	@Override
 	protected void setTypeInfo() {
-		typeInfo=new TupleTypeInfo<Tuple2<OUT1, OUT2>>(
-				outTypeWrapper1.getTypeInfo(),
-				outTypeWrapper2.getTypeInfo()
-		);
+		typeInfo = new TupleTypeInfo<Tuple2<OUT1, OUT2>>(
+				outTypeWrapper1.getTypeInfo(), outTypeWrapper2.getTypeInfo());
 	}
 }
