03cd30a112d3715bb841517ddc3dbb190b4ce93d
Jonas Traub (powibol) <jon@s-traub.com>
2014-10-09 14:34:14 +0200

    removed unused fields

--- flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/invokable/operator/NextGenDeltaPolicy.java
+++ flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/invokable/operator/NextGenDeltaPolicy.java
@@ -10,23 +10,15 @@ public class NextGenDeltaPolicy<DATA> implements NextGenTriggerPolicy<DATA>, Nex
      */
     private static final long serialVersionUID = -7797538922123394967L;
 
-    private static final int DEFAULT_DELETE_ON_EVICTION = 1;
-
     private NextGenDeltaFunction<DATA> deltaFuntion;
     private List<DATA> windowBuffer;
     private int threshold;
-    private int deleteOnEviction;
     private DATA triggerDataPoint;
 
     public NextGenDeltaPolicy(NextGenDeltaFunction<DATA> deltaFuntion, DATA init, int threshold) {
-        this(deltaFuntion, init, threshold, DEFAULT_DELETE_ON_EVICTION);
-    }
-
-    public NextGenDeltaPolicy(NextGenDeltaFunction<DATA> deltaFuntion, DATA init, int threshold, int deleteOnEviction) {
-        this.deltaFuntion = deltaFuntion;
+    	this.deltaFuntion = deltaFuntion;
         this.windowBuffer = new LinkedList<DATA>();
         this.threshold = threshold;
-        this.deleteOnEviction = deleteOnEviction;
     }
 
     @Override
