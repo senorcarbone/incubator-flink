d7e958d90c44a38a1d0ab3e73938073c7686c33b
Jonas Traub (powibol) <jon@s-traub.com>
2014-10-20 10:57:32 +0200

    Removed warnings/organize imports

--- flink-addons/flink-streaming/flink-streaming-core/src/test/java/org/apache/flink/streaming/api/invokable/operator/WindowingInvokableTest.java
+++ flink-addons/flink-streaming/flink-streaming-core/src/test/java/org/apache/flink/streaming/api/invokable/operator/WindowingInvokableTest.java
@@ -3,7 +3,6 @@ package org.apache.flink.streaming.api.invokable.operator;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
