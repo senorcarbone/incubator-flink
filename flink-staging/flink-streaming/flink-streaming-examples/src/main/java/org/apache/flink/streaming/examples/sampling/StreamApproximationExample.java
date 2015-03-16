e75cfb83ae4136f2115782c7b725553b95a4286a
marthavk <martha.1@hotmail.com>
2015-03-16 09:47:31 +0100

    minor changes in StreamApproximationExample

--- flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/sampling/StreamApproximationExample.java
+++ flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/sampling/StreamApproximationExample.java
@@ -1,8 +1,11 @@
 package org.apache.flink.streaming.examples.sampling;
 
+import org.apache.commons.math3.stat.inference.TestUtils;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.uncommons.maths.random.GaussianGenerator;
+//import org.apache.commons.math3.distribution.*;
 
 import java.util.Random;
 
@@ -37,9 +40,28 @@ public class StreamApproximationExample {
         });
     }
 
-    private static DataStream<Double> createGaussianEvolvingStream(StreamApproximationExample env, int length) {
+    private static DataStream<Double> createGaussianEvolvingStream(StreamExecutionEnvironment env, final int length) {
+        final double mean1 = 0;
+        final double mean2 = 10;
+        final double stdev1 = 1;
+        final double stdev2 = 3;
+        final Random rand = new Random();
         //TODO
-        return null;
+        DataStream<Long> stream = env.generateSequence(1, length);
+        return stream.map(new MapFunction<Long, Double>() {
+            long counter = 0;
+            @Override
+            public Double map(Long value) throws Exception {
+                double meanStep = (mean2-mean1)/length;
+                double stdevStep = (stdev2-stdev1)/length;
+                double nextMean = mean1 + counter*meanStep;
+                double nextStdev = stdev1 + counter*stdevStep;
+                counter ++;
+                return rand.nextGaussian()*nextStdev+nextMean;
+            }
+        });
+
+
     }
 
 
