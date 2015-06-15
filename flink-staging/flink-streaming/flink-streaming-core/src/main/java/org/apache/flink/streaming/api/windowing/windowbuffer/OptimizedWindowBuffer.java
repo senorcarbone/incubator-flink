fc8c21f450b7dbd332c48ab42d7319cb5534060d
Jonas Traub <jon@s-traub.com>
2015-06-15 13:47:44 +0200

    [streaming] Added OptimizedWindowBuffer prototype

---
+++ flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/windowing/windowbuffer/OptimizedWindowBuffer.java
@@ -0,0 +1,199 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.api.windowing.windowbuffer;
+
+import org.apache.flink.api.common.functions.ReduceFunction;
+
+import java.util.*;
+
+public class OptimizedWindowBuffer<T> {
+
+    private Map<Integer,Integer> windowToStartId = new HashMap<Integer,Integer>();
+    private Map<Integer,Integer> queryToStartId = new HashMap<Integer,Integer>();
+    private LinkedList<Integer> preAggregationStartIds = new LinkedList<Integer>();
+    private LinkedList<T> preAggregations = new LinkedList<T>();
+    private LinkedList<T> individualTuples = new LinkedList<T>();
+    private int nextSeqId=0;
+    private int nextQueryId=0;
+    private int nextWindowId=0;
+    private boolean keepIndvidualTuples;
+    private boolean doPreAggregation;
+    private boolean startNewPreAggregation = true;
+    private ReduceFunction<T> reduceFunction;
+
+    public OptimizedWindowBuffer(ReduceFunction<T> reduceFunction, boolean doPreAggregation, boolean keepIndvidualTuples){
+        this.keepIndvidualTuples=keepIndvidualTuples;
+        this.doPreAggregation=doPreAggregation;
+        this.reduceFunction=reduceFunction;
+    }
+
+    public void store(final T element) throws Exception {
+        //Handle individual tuples
+        if (keepIndvidualTuples){
+            individualTuples.add(element);
+        }
+
+        //Handle pre-aggregation
+        if (doPreAggregation){
+            //If border start new pre-aggregation
+            if (startNewPreAggregation || preAggregations.isEmpty()) {
+                preAggregationStartIds.add(nextSeqId);
+                preAggregations.add(element);
+                this.startNewPreAggregation=false;
+            } else {
+                T newValue = this.reduceFunction.reduce(preAggregations.getLast(), element);
+                preAggregations.removeLast();
+                preAggregations.add(newValue);
+            }
+        }
+
+        //increase sequence id
+        this.nextSeqId++;
+
+        //handle overflow of sequence id
+        if (nextSeqId==Integer.MAX_VALUE){
+            handleSequenceIdOverflow();
+        }
+    }
+
+    public void evict(int n, int queryId) {
+        queryToStartId.replace(queryId,queryToStartId.get(queryId)+n);
+        deleteExpired();
+    }
+
+    public T emitWindow(int queryId) throws Exception{
+        int startId=queryToStartId.get(queryId);
+        Iterator<Integer> descendingIterator=preAggregationStartIds.descendingIterator();
+        T result=null;
+        int counter = preAggregationStartIds.size()-1;
+        int lastPreAggStart=nextSeqId;
+
+        //Iteration threw the pre-aggregations
+        if (doPreAggregation) {
+            while (descendingIterator.hasNext()) {
+                int preAggStart = descendingIterator.next();
+                if (preAggStart >= startId) {
+                    lastPreAggStart = preAggStart;
+                    if (result == null) {
+                        result = preAggregations.get(counter);
+                    } else {
+                        result = this.reduceFunction.reduce(preAggregations.get(counter),result);
+                    }
+                    counter--;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        //Filling the gap from window start to start of the used pre aggregation
+        int start=individualTuples.size()-(nextSeqId-startId);
+        int end=individualTuples.size()-(nextSeqId-lastPreAggStart);
+
+        for (int i=start;i<end;i++){
+            if (result==null) {
+                result=individualTuples.get(i);
+            } else {
+                result = this.reduceFunction.reduce(result,individualTuples.get((int) i));
+            }
+        }
+
+        return result;
+    }
+
+    public int startWindow() {
+        this.windowToStartId.put(this.nextWindowId,this.nextSeqId);
+        this.startNewPreAggregation=true;
+        return nextWindowId++;
+    }
+
+    public T endWindow(int windowId) throws Exception {
+        long start=this.windowToStartId.get(windowId);
+        this.windowToStartId.remove(windowId);
+
+        //catch empty windows
+        if (start==nextSeqId){
+            return null;
+        }
+
+        //reduce pre-aggregations to final result
+        T result=preAggregations.getLast();
+        int counter=preAggregations.size()-2; //last already fetched
+        while (counter>=0 && preAggregationStartIds.get(counter)>=start){
+            result=this.reduceFunction.reduce(preAggregations.get(counter),result);
+            counter--;
+        }
+
+        deleteExpired();
+
+        return result;
+    }
+
+    public int registerQuery() {
+        this.queryToStartId.put(nextQueryId,nextSeqId);
+        return this.nextQueryId++;
+    }
+
+    public void deregisterQuery(int id) {
+        this.queryToStartId.remove(id);
+        deleteExpired();
+    }
+
+    public OptimizedWindowBuffer<T> clone() {
+        //TODO If needed: Implement this!
+        return null;
+    }
+
+    private void handleSequenceIdOverflow(){
+        //TODO this class will fail in case more than MAX_INT tuples arrive
+        throw new RuntimeException("The sequence id reached the limit given by the type long!");
+    }
+
+    private void deleteExpired(){
+        //Collect current eviction position
+        Collection<Integer> queryBufferStarts= queryToStartId.values();
+        Collection<Integer> windowBufferStarts= windowToStartId.values();
+
+        //What is the oldest required tuple?
+        int minimum=Integer.MAX_VALUE;
+        for (int current:queryBufferStarts){
+            if (current<minimum){
+                minimum=current;
+            }
+        }
+        for (int current:windowBufferStarts){
+            if (current<minimum){
+                minimum=current;
+            }
+        }
+
+        //Delete all older than the minimum
+        while (!preAggregations.isEmpty() && preAggregationStartIds.getFirst()<minimum){
+            preAggregationStartIds.removeFirst();
+            preAggregations.removeFirst();
+        }
+
+        if (!individualTuples.isEmpty()) {
+            long numToDelete = individualTuples.size() - (nextSeqId - minimum);
+            for (int i = 0; i < numToDelete; i++) {
+                individualTuples.removeFirst();
+            }
+        }
+    }
+
+}
---
+++ flink-staging/flink-streaming/flink-streaming-core/src/test/java/org/apache/flink/streaming/api/windowing/windowbuffer/OptimizedWindowBufferTest.java
@@ -0,0 +1,159 @@
+package org.apache.flink.streaming.api.windowing.windowbuffer;
+
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple1;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class OptimizedWindowBufferTest {
+
+    @Test
+    public void testDeterministic() throws Exception {
+
+        OptimizedWindowBuffer<Tuple1<String>> optimizedWindowBuffer = new OptimizedWindowBuffer<Tuple1<String>>(new TestReduceFunction(),true,false);
+
+        //Try single window
+        int window1 = optimizedWindowBuffer.startWindow();
+        optimizedWindowBuffer.store(new Tuple1<String>("1"));
+        optimizedWindowBuffer.store(new Tuple1<String>("2"));
+        optimizedWindowBuffer.store(new Tuple1<String>("3"));
+        Tuple1<String> window1Result = optimizedWindowBuffer.endWindow(window1);
+        compareStrings("1|2|3", window1Result.f0);
+
+        //Try multiple overlapping windows
+        optimizedWindowBuffer.store(new Tuple1<String>("1"));
+        optimizedWindowBuffer.store(new Tuple1<String>("2"));
+        int window2 = optimizedWindowBuffer.startWindow();
+        int window3 = optimizedWindowBuffer.startWindow();
+        optimizedWindowBuffer.store(new Tuple1<String>("3"));
+        optimizedWindowBuffer.store(new Tuple1<String>("4"));
+        Tuple1<String> window3Result = optimizedWindowBuffer.endWindow(window3);
+        compareStrings("3|4", window3Result.f0);
+        optimizedWindowBuffer.store(new Tuple1<String>("5"));
+        int window4 = optimizedWindowBuffer.startWindow();
+        optimizedWindowBuffer.store(new Tuple1<String>("6"));
+        Tuple1<String> window2Result = optimizedWindowBuffer.endWindow(window2);
+        compareStrings("3|4|5|6", window2Result.f0);
+        optimizedWindowBuffer.store(new Tuple1<String>("7"));
+        optimizedWindowBuffer.store(new Tuple1<String>("8"));
+        optimizedWindowBuffer.store(new Tuple1<String>("9"));
+        Tuple1<String> window4Result = optimizedWindowBuffer.endWindow(window4);
+        compareStrings("6|7|8|9", window4Result.f0);
+
+        //Try empty window
+        int window5 = optimizedWindowBuffer.startWindow();
+        Tuple1<String> window5Result = optimizedWindowBuffer.endWindow(window5);
+        assertNull(window5Result);
+    }
+
+    @Test
+    public void testNotDeterministic() throws Exception{
+
+        OptimizedWindowBuffer<Tuple1<String>> optimizedWindowBuffer = new OptimizedWindowBuffer<Tuple1<String>>(new TestReduceFunction(),false,true);
+
+        //Try single window
+        int query1 = optimizedWindowBuffer.registerQuery();
+        optimizedWindowBuffer.store(new Tuple1<String>("1"));
+        optimizedWindowBuffer.store(new Tuple1<String>("2"));
+        optimizedWindowBuffer.store(new Tuple1<String>("3"));
+        Tuple1<String> window1Result = optimizedWindowBuffer.emitWindow(query1);
+        compareStrings("1|2|3", window1Result.f0);
+
+        //Try multiple overlapping windows
+        optimizedWindowBuffer.evict(3,query1);
+        optimizedWindowBuffer.store(new Tuple1<String>("1"));
+        optimizedWindowBuffer.store(new Tuple1<String>("2"));
+        optimizedWindowBuffer.evict(2,query1);
+        int query2 = optimizedWindowBuffer.registerQuery();
+        optimizedWindowBuffer.store(new Tuple1<String>("3"));
+        optimizedWindowBuffer.store(new Tuple1<String>("4"));
+        Tuple1<String> window3Result = optimizedWindowBuffer.emitWindow(query2);
+        compareStrings("3|4", window3Result.f0);
+        optimizedWindowBuffer.store(new Tuple1<String>("5"));
+        optimizedWindowBuffer.store(new Tuple1<String>("6"));
+        Tuple1<String> window2Result = optimizedWindowBuffer.emitWindow(query1);
+        compareStrings("3|4|5|6", window2Result.f0);
+        optimizedWindowBuffer.store(new Tuple1<String>("7"));
+        optimizedWindowBuffer.store(new Tuple1<String>("8"));
+        optimizedWindowBuffer.store(new Tuple1<String>("9"));
+        optimizedWindowBuffer.evict(3,query1);
+        Tuple1<String> window4Result = optimizedWindowBuffer.emitWindow(query1);
+        compareStrings("6|7|8|9", window4Result.f0);
+
+        //Try empty window
+        int window5 = optimizedWindowBuffer.startWindow();
+        Tuple1<String> window5Result = optimizedWindowBuffer.endWindow(window5);
+        assertNull(window5Result);
+
+    }
+
+    @Test
+    public void testMixed() throws Exception{
+
+        OptimizedWindowBuffer<Tuple1<String>> optimizedWindowBuffer = new OptimizedWindowBuffer<Tuple1<String>>(new TestReduceFunction(),true,true);
+
+        //Try single window
+        int window1 = optimizedWindowBuffer.startWindow();
+        int query1 = optimizedWindowBuffer.registerQuery();
+        optimizedWindowBuffer.store(new Tuple1<String>("1"));
+        optimizedWindowBuffer.store(new Tuple1<String>("2"));
+        optimizedWindowBuffer.store(new Tuple1<String>("3"));
+        Tuple1<String> window1Result = optimizedWindowBuffer.endWindow(window1);
+        compareStrings("1|2|3", window1Result.f0);
+        Tuple1<String> window6Result = optimizedWindowBuffer.emitWindow(query1);
+        compareStrings("1|2|3", window6Result.f0);
+
+        //Try multiple overlapping windows
+        optimizedWindowBuffer.evict(3,query1);
+        optimizedWindowBuffer.store(new Tuple1<String>("1"));
+        optimizedWindowBuffer.store(new Tuple1<String>("2"));
+        optimizedWindowBuffer.evict(2,query1);
+        int window2 = optimizedWindowBuffer.startWindow();
+        int window3 = optimizedWindowBuffer.startWindow();
+        int query2 = optimizedWindowBuffer.registerQuery();
+        optimizedWindowBuffer.store(new Tuple1<String>("3"));
+        optimizedWindowBuffer.store(new Tuple1<String>("4"));
+        Tuple1<String> window7Result = optimizedWindowBuffer.emitWindow(query2);
+        compareStrings("3|4", window7Result.f0);
+        Tuple1<String> window3Result = optimizedWindowBuffer.endWindow(window3);
+        compareStrings("3|4", window3Result.f0);
+        optimizedWindowBuffer.store(new Tuple1<String>("5"));
+        int window4 = optimizedWindowBuffer.startWindow();
+        optimizedWindowBuffer.store(new Tuple1<String>("6"));
+        Tuple1<String> window2Result = optimizedWindowBuffer.endWindow(window2);
+        compareStrings("3|4|5|6", window2Result.f0);
+        Tuple1<String> window8Result = optimizedWindowBuffer.emitWindow(query1);
+        compareStrings("3|4|5|6", window8Result.f0);
+        optimizedWindowBuffer.store(new Tuple1<String>("7"));
+        optimizedWindowBuffer.store(new Tuple1<String>("8"));
+        optimizedWindowBuffer.store(new Tuple1<String>("9"));
+        Tuple1<String> window4Result = optimizedWindowBuffer.endWindow(window4);
+        compareStrings("6|7|8|9", window4Result.f0);
+        optimizedWindowBuffer.evict(3,query1);
+        Tuple1<String> window9Result = optimizedWindowBuffer.emitWindow(query1);
+        compareStrings("6|7|8|9", window9Result.f0);
+
+        //Try empty window
+        int window5 = optimizedWindowBuffer.startWindow();
+        Tuple1<String> window5Result = optimizedWindowBuffer.endWindow(window5);
+        assertNull(window5Result);
+    }
+
+    /**
+     * This reduce function concatenates the inputs it receives.
+     * Inputs are seperated by pipes.
+     */
+    private class TestReduceFunction implements ReduceFunction<Tuple1<String>>{
+
+        @Override
+        public Tuple1<String> reduce(Tuple1<String> value1, Tuple1<String> value2) throws Exception {
+            return new Tuple1<String>(value1.f0+"|"+value2.f0);
+        }
+    }
+
+    private void compareStrings(String expected, String actual){
+        assertEquals("Wrong output: expected: "+expected+" actual: "+actual,expected,actual);
+    }
+}
