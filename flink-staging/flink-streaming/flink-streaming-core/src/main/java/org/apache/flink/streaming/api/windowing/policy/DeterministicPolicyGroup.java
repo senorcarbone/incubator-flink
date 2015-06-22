f37c58adefa91fb1f37accc003a151d3a97d3c0d
Jonas Traub <jon@s-traub.com>
2015-06-22 15:26:40 +0200

    Added implementation of policy groups including test case

---
+++ flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/windowing/policy/DeterministicPolicyGroup.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.api.windowing.policy;
+
+import org.apache.flink.streaming.api.windowing.extractor.Extractor;
+import org.apache.flink.streaming.api.windowing.extractor.FieldFromTuple;
+
+import java.util.LinkedList;
+
+/**
+ * A policy group wraps around several deterministig windowing policies which operate on the same field of the input tuples.
+ * @param <DATA> the type of input tuples handled by this policy group
+ */
+public class DeterministicPolicyGroup<DATA> {
+
+    private DeterministicTriggerPolicy<DATA> trigger;
+    private DeterministicEvictionPolicy<DATA> eviction;
+    private Extractor<DATA,Double> fieldExtractor;
+    private LinkedList<Double> windowStartLookahead = new LinkedList<Double>();
+    private LinkedList<Double> windowEndLookahead = new LinkedList<Double>();
+    private double latestTriggerPosition=Double.MIN_VALUE;
+
+    /**
+     * This constructer sets up the policy group in case Tuple-types are used.
+     * NOTE: If DATA is not instance of Tuple, the usage of this constructor will cause an exception at runtime.
+     * @param trigger The deterministic trigger policy of this group
+     * @param eviction The deterministic eviction policy of this group
+     * @param keyFieldId The id of the field in the input tuple on which the policies operate
+     */
+    @SuppressWarnings("unchecked, unused")
+    public DeterministicPolicyGroup(DeterministicTriggerPolicy<DATA> trigger, DeterministicEvictionPolicy<DATA> eviction, int keyFieldId){
+        this(trigger,eviction);
+        this.fieldExtractor=new FieldFromTuple(keyFieldId);
+    }
+
+    /**
+     * This constructor sets up the policy group.
+     * It can handle arbitrary input types using the provided key selector.
+     * @param trigger The deterministic trigger policy of this group
+     * @param eviction The deterministic eviction policy of this group
+     * @param fieldExtractor A key selector which selects the field from the input on which the policies operate
+     */
+    @SuppressWarnings("unchecked, unused")
+    public DeterministicPolicyGroup(DeterministicTriggerPolicy<DATA> trigger, DeterministicEvictionPolicy<DATA> eviction, Extractor fieldExtractor){
+        this(trigger,eviction);
+        this.fieldExtractor = fieldExtractor;
+    }
+
+    /**
+     * This constructor can only be used if the type of DATA is Double already.
+     * No extractor will be applied if this constructor is used.
+     * (Just to prevent duplicated code)
+     * @param trigger The deterministic trigger policy of this group
+     * @param eviction The deterministic eviction policy of this group
+     */
+    public DeterministicPolicyGroup(DeterministicTriggerPolicy<DATA> trigger, DeterministicEvictionPolicy<DATA> eviction){
+        this.trigger=trigger;
+        this.eviction=eviction;
+    }
+
+    /**
+     * This method is used to obtain the window events which are caused by the arrived of the tuple wich is given as parameter.
+     * @param tuple The currently arrives input tuple
+     * @return The window events caused by the arrival of this tuple: 0 if no event occurs otherwise number of opened
+     * windows in the first 16bit, number of closed windows in the last 16bit.
+     * To get the window begin counter use: (result >> 16)
+     * To get the window end counter use: (result & 0xFFFF)
+     */
+    public int getWindowEvents(DATA tuple){
+
+        double position;
+
+        //Extract the required field from the tuple
+        if (fieldExtractor==null){
+            //If an exception is thrown here, neither DATA is instance of Double nor a correct extractor has been set.
+            position=(Double)tuple;
+        } else {
+            position=this.fieldExtractor.extract(tuple);
+        }
+
+        //Extend the look ahead such that we know a complete window beyond the position of the current tuple
+        while (windowStartLookahead.isEmpty() || windowStartLookahead.getLast()<=position){
+            windowEndLookahead.add(latestTriggerPosition = trigger.getNextTriggerPosition(latestTriggerPosition));
+            windowStartLookahead.add(eviction.getLowerBorder(latestTriggerPosition));
+        }
+
+        //Calculate the number of window begins
+        short windowBeginCounter=0;
+        while (!windowStartLookahead.isEmpty() && windowStartLookahead.getFirst()<=position){
+            windowStartLookahead.removeFirst();
+            windowBeginCounter++;
+        }
+
+        //Calculate the number of window ends
+        short windowEndCounter=0;
+        while (!windowEndLookahead.isEmpty() && windowEndLookahead.getFirst()<=position){
+            windowEndLookahead.removeFirst();
+            windowEndCounter++;
+        }
+
+        //Combine and return counter
+        return (windowBeginCounter<<16)+windowEndCounter;
+    }
+
+}
---
+++ flink-staging/flink-streaming/flink-streaming-core/src/test/java/org/apache/flink/streaming/api/windowing/policy/DeterministicPolicyGroupTest.java
@@ -0,0 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.api.windowing.policy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+public class DeterministicPolicyGroupTest {
+
+    //Set window begin and end positions
+    private static double[] windowBegins={0d,1d,3d,5d,5d,10d,98d};
+    private static double[] windowEnds={5d,7d,7d,8d,10d,15d,99d};
+
+    //Set the numbers of opened and closed windows for each seq. number
+    //                                          0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+    private static short[] windowBeginCounters={1,1,0,1,0,2,0,0,0,0, 1, 0, 0, 0, 0, 0};
+    private static short[] windowEndCounters = {0,0,0,0,0,1,0,2,1,0, 1, 0, 0, 0, 0, 1};
+
+    /**
+     * This test assumes the following windows:
+     * 0-5,1-7,3-7,5-8,5-10,10-15
+     *
+     * Window begins are at: 0,1,3,5,5,10
+     * Window ends are at: 5,7,7,8,10,15
+     */
+    @Test
+    public void runTest(){
+
+        LinkedList<Integer> results=new LinkedList<Integer>();
+
+        DeterministicTriggerPolicy<Double> trigger=new DeterministicSequenceTrigger<Double>();
+        DeterministicEvictionPolicy<Double> evictor=new DeterministicSequenceEvictor<Double>();
+        DeterministicPolicyGroup<Double> group = new DeterministicPolicyGroup<Double>(trigger,evictor);
+
+        for (double i=0;i<=15; i++){
+            results.add(group.getWindowEvents(i));
+        }
+
+        for (int i=0;i<=15; i++){
+            assertEquals("The window begin counter at position "+i+" was wrong.",windowBeginCounters[i],results.get(i)>>16);
+            assertEquals("The window end counter at position "+i+" was wrong.",windowEndCounters[i],results.get(i)&0xFFFF);
+        }
+
+    }
+
+    /**
+     * A simple deterministic trigger policy, only for testing.
+     * @param <DATA> The type of input data handled by this policy
+     */
+    private class DeterministicSequenceTrigger<DATA> implements DeterministicTriggerPolicy<DATA>{
+
+        int counter=0;
+
+        @Override
+        public double getNextTriggerPosition(double previouseTriggerPosition) {
+            if (windowEnds[counter]>=previouseTriggerPosition){
+                return windowEnds[counter++];
+            }
+            throw new RuntimeException("Unexpected value of previouseTriggerPosition.");
+        }
+
+        @Override
+        public boolean notifyTrigger(DATA datapoint) {
+            throw new UnsupportedOperationException("This policy was implemented for testing purposes only. This method should never be called!");
+        }
+    }
+
+    /**
+     * A simple deterministic eviction policy, only for testing.
+     * @param <DATA> The type of input data handled by this policy
+     */
+    private class DeterministicSequenceEvictor<DATA> implements DeterministicEvictionPolicy<DATA>{
+
+        int counter=0;
+
+        @Override
+        public double getLowerBorder(double upperBorder) {
+            if (windowEnds[counter]==upperBorder){
+                return windowBegins[counter++];
+            } else {
+                throw new RuntimeException("Wrong upper border: "+upperBorder);
+            }
+        }
+
+        @Override
+        public int notifyEviction(DATA datapoint, boolean triggered, int bufferSize) {
+            throw new UnsupportedOperationException("This policy was implemented for testing purposes only. This method should never be called!");
+        }
+    }
+
+}
